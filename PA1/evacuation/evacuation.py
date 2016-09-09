f06d9a824e05ef91a2af1943b2848813dd048c80
androidwargames <adwagner89@gmail.com>
2016-09-08 21:02:37 -0500

    Finished Evac

--- PA1/evacuation/evacuation.py
+++ PA1/evacuation/evacuation.py
@@ -58,12 +58,88 @@ def read_data():
     return graph
 
 
+def read_data_debug(a):
+    vertex_count, edge_count = map(int, a[0].split())
+    graph = FlowGraph(vertex_count)
+    for _ in range(edge_count):
+        u, v, capacity = map(int, a[_+1].split())
+        graph.add_edge(u - 1, v - 1, capacity)
+    return graph
+
+
 def max_flow(graph, from_, to):
+    done = True
     flow = 0
-    # your code goes here
+    while done:
+        new = [True] * len(graph.graph)
+        queue = [[from_,-1, -1]]
+        new[from_] = False
+        i = 0
+        done = False
+        while i < len(queue):
+            a = graph.get_ids(queue[i][0])
+            for j in a:
+                e = graph.get_edge(j)
+                if e.capacity - e.flow > 0 and new[e.v]:
+                    queue.append([e.v, i, j])
+                    new[e.v] = False
+                    if e.v == to:
+                        done = True
+                        break
+            if done:
+                clean_ids(graph, queue)
+                break
+            i += 1
+    for i in graph.get_ids(to):
+        if i % 2 == 1:
+            flow += graph.get_edge(i-1).flow
     return flow
 
 
+def clean_ids(graph, queue):
+    i = len(queue) - 1
+    path = []
+    edges = []
+    while i > 0:
+        path.append(queue[i][2])
+        edges.append(graph.get_edge(queue[i][2]))
+        i = queue[i][1]
+
+    path.reverse()
+    edges.reverse()
+
+    min = 100000000
+    for i in edges:
+        a = i.capacity - i.flow
+        if a < min:
+            min = a
+    for i in path:
+        graph.add_flow(i, min)
+
+
+
+# this code allows automated testing of the three sample tests, rather than relying on the standard in
+"""
+import os
+dir_path = os.path.dirname(os.path.realpath(__file__)) + '/tests/'
+for i in os.listdir(dir_path):
+    if i.find('.a') == -1:
+        print(i)
+        with open(dir_path + i, 'r') as f:
+            content = f.readlines()
+        f.close()
+        f = open(dir_path + i + '.a', 'r')
+        c = f.read()
+        c = int(c)
+        graph = read_data_debug(content)
+        d = max_flow(graph, 0, graph.size() - 1)
+        if d == c:
+            print(True)
+        else:
+            print([c,d])
+        f.close()
+exit(14)
+"""
 if __name__ == '__main__':
     graph = read_data()
     print(max_flow(graph, 0, graph.size() - 1))
